739436b64a30a9c15506059dc9df30fb26c9cef5
Victoria Breen <victoriabreen8@hotmail.com>
2025-02-19 10:44:07 -0330

    Adding UserRepository

--- src/main/java/com/socialpetwork/user/UserRepository.java
+++ src/main/java/com/socialpetwork/user/UserRepository.java
@@ -1,4 +1,16 @@
-package com.socialpetwork.User;
+package com.socialpetwork.user;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+    // I picked these as they are unique, but I can change or add
+    // Find user by email and Check if email exist
+    User findByEmail(String email);
+    boolean existsByEmail(String email);
+
+    // Find user by username and check if exist
+    User findByUsername(String username);
+    boolean existsByUsername(String username);
+
 }
