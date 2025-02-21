c5789635a98f188e1745c4b0e50d532d998bbf13
Victoria Breen <victoriabreen8@hotmail.com>
2025-02-21 17:51:59 -0330

    Adding comments code

--- src/main/java/com/socialpetwork/comment/CommentRepository.java
+++ src/main/java/com/socialpetwork/comment/CommentRepository.java
@@ -1,4 +1,13 @@
 package com.socialpetwork.comment;
 
-public class CommentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findFromPostId(Long postId);
+    List<Comment> findFromUserId(Long userId);
 }
+
