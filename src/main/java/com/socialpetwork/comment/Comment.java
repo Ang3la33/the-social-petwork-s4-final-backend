45982863e41bb910dae2767577bd4ce090f0f300
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-02-24 17:07:52 -0330

    Added no-arg constructor to Comment class

--- src/main/java/com/socialpetwork/comment/Comment.java
+++ src/main/java/com/socialpetwork/comment/Comment.java
@@ -32,6 +32,11 @@ public class Comment {
         this.postedAt = (postedAt != null) ? postedAt : LocalDateTime.now();
     }
 
+    // No-arg constructor
+    public Comment() {
+
+    }
+
     // Getters and Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
