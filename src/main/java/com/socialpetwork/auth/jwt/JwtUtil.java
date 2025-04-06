3cd12150d09ba0eb7ec76774795c0f04fb7cbd77
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-04-06 20:26:02 -0230

    Set up JWT utility class that generates JWTs when user logs in, validates incoming JWTs for secure requests and then extracts the username and role from a token

--- src/main/java/com/socialpetwork/auth/jwt/JwtUtil.java
+++ src/main/java/com/socialpetwork/auth/jwt/JwtUtil.java
@@ -3,8 +3,58 @@ package com.socialpetwork.auth.jwt;
 import com.socialpetwork.entity.UserType;
 import io.jsonwebtoken.*;
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Date;
 
+@Component
 public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    // Set jwt expiration to 1 day
+    private final long jwtExpirationInMs = 86400000;
+
+    // Generate JWT with username and role
+    public String generateToken(String username, UserType role) {
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("role", role.name())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationInMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    // Extract username (subject) from token
+    public String getUsernameFromToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJwt(token)
+                .getBody()
+                .getSubject();
+    }
+
+    // Extract role from token
+    public String getRoleFromToken(String token) {
+        return (String) Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJwt(token)
+                .getBody()
+                .get("role");
+    }
+
+    // Validate token structure and expiration
+    public boolean validateToken(String token) {
+        try{
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        }
+        catch (SignatureException | MalformedJwtException | ExpiredJwtException | UnsupportedJwtException | IllegalArgumentException e) {
+            System.out.println("Invalid JWT");
+            return false;
+        }
+    }
+
 }
