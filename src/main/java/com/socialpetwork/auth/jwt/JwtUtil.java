444e700f8e66a9f3b01fbf2934e589325685fff9
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-04-06 19:54:27 -0230

    Added JSON web token dependency for use in JwtUtil class

---
+++ src/main/java/com/socialpetwork/auth/jwt/JwtUtil.java
@@ -0,0 +1,10 @@
+package com.socialpetwork.auth.jwt;
+
+import com.socialpetwork.entity.UserType;
+import io.jsonwebtoken.*;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+public class JwtUtil {
+}
