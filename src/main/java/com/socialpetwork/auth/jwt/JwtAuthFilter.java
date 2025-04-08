550f15fe5eb4e18342c99798e837aa998b8fbdf2
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-04-07 23:32:50 -0230

    Added role checking for authentication

--- src/main/java/com/socialpetwork/auth/jwt/JwtAuthFilter.java
+++ src/main/java/com/socialpetwork/auth/jwt/JwtAuthFilter.java
@@ -5,6 +5,7 @@ import com.socialpetwork.repository.UserRepository;
 import io.jsonwebtoken.JwtException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
@@ -16,6 +17,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
@@ -36,9 +39,11 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             try {
                 if (jwtUtil.validateToken(token)) {
                     String username = jwtUtil.getUsernameFromToken(token);
+                    String role = jwtUtil.getRoleFromToken(token);
                     User user = userRepository.findByUsername(username);
 
                     if (user != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                        List<SimpleGrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_" + role));
                         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user,null,null);
                         authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                         SecurityContextHolder.getContext().setAuthentication(authenticationToken);
