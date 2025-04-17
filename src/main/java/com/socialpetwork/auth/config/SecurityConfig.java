d7079541f4589a201a6d6e3344d1e15c1c37f372
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-04-16 23:17:21 -0230

    updated localhost to RDS route

--- src/main/java/com/socialpetwork/auth/config/SecurityConfig.java
+++ src/main/java/com/socialpetwork/auth/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://99.79.59.205:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(Boolean.valueOf(true));
--- src/main/java/com/socialpetwork/auth/config/WebConfig.java
+++ src/main/java/com/socialpetwork/auth/config/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://99.79.59.205:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
