79d16030e1dfbf55473b9d3e7643edf78c6e788d
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-04-17 13:01:58 -0230

    updated cors

--- src/main/java/com/socialpetwork/auth/config/WebConfig.java
+++ src/main/java/com/socialpetwork/auth/config/WebConfig.java
@@ -19,8 +19,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://99.79.59.205:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins("*")
+                .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
